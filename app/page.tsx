89e9e23dc26af0e0c0c818739a874016207b6708
NanPar22 <Chaiyanan22102002@gmail.com>
2025-12-09 09:09:15 +0700

    update/index

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,19 @@ export default function Home() {
   return (
     <div className="flex  flex-col gap-1 min-h-screen w-full text-black font-sans ">
       {/* โปรเตอร์ */}
-      <div className="pt-15 h-150 w-full via-fuchsia-700 text-blac bg-white">
-        <div className="flex justify-center items-center  text-3xl">
-          <h1>สินค้ายอดฮิต</h1>
+      <div className=" h-150 w-full via-fuchsia-700 text-blac bg-white flex justify-center pt-12">
+        <div className="flex h-140 w-[1600px]  ">
+          <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/34b5bf180145769.6505ae7623131.jpg"
+            className="object-cover object-top  h-full w-full  "
+            alt="" />
+        </div>
       </div>
-      </div>
-      <div className="p-2 h-150 w-full via-fuchsia-700 text-blac bg-white">
-        <div className="flex justify-center items-center  ">
-          <h1 >สินค้ายอดฮิต</h1>
-      </div>
+      <div className="pt-4   h-150 w-full via-fuchsia-700 text-blac bg-white ">
+        <div className="flex flex-col justify-center items-center gap-5 h-full ">
+          <h1 className="font_thai" >สินค้ายอดฮิต</h1>
+          <div className=" h-full w-full bg-amber-200 ">
+          </div>
+        </div>
       </div>
       <div className="h-150 w-full via-fuchsia-700 text-blac bg-white">da</div>
       <div className="h-150 w-full via-fuchsia-700 text-blac bg-white">dsa</div>
